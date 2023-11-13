7573e0939638fa84f4142477eab0049d3ae0d9ef
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-11-12 20:12:22 -0500

    Refactor OpenAI queries

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -1,13 +1,14 @@
 import os
+from abc import ABC
+
 import openai
+import requests
 from tenacity import (
     retry,
     stop_after_attempt,
     wait_random_exponential,
 )
-from transformers import AutoTokenizer
-import transformers
-import torch
+
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -20,33 +21,79 @@ LLAMA2_TEMPLATE = """<s>[INST] <<SYS>>
 """
 
 
-class VLEODataset:
-    model = "meta-llama/Llama-2-70b-chat-hf"
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    pipeline = transformers.pipeline(
-        "text-generation",
-        model=model,
-        torch_dtype=torch.float16,
-        device_map="auto",
-    )
+class VLEODataset(ABC):
+    model = None
+    tokenizer = None
+    pipeline = None
+    system_message = ("You are a helpful image analyst that specializes in satellite and aerial images. You always "
+                      "truthfully answer the user's question. If you are not sure about something, don't answer false"
+                      " information.")
 
     def download_url(self, url: str):
         pass
 
     @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
-    def request_openai(self, system: str, user: str):
-        return openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[
-                {"role": "system", "content": system},
-                {"role": "user", "content": user}
-            ],
-            top_p=0.1,
-            temperature=0.2,
-            max_tokens=256
-        )
+    def query_openai(self, base64_image: str, system: str, user: str):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {os.environ['OPENAI_API_KEY']}",
+            "OpenAI-Organization": os.environ["OPENAI_API_ORG"]
+        }
 
-    def request_loca_llama(self, system: str, user: str):
+        payload = {
+            "model": "gpt-4-vision-preview",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": system
+                        }
+                    ]
+                },
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": user
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": f"data:image/jpeg;base64,{base64_image}",
+                                "detail": "high"
+                            }
+                        }
+                    ]
+                }
+            ],
+            "max_tokens": 300,
+            "temperature": 0.2
+        }
+
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+        if not response.ok:
+            raise IOError(str(response.json()))
+
+        return payload, response.json()
+
+    def request_loca_llama(self, model: str, system: str, user: str):
+        from transformers import AutoTokenizer
+        import transformers
+        import torch
+
+        if self.pipeline is None or model != self.model:
+            self.model = model
+            self.tokenizer = AutoTokenizer.from_pretrained(model)
+            self.pipeline = transformers.pipeline(
+                "text-generation",
+                model=model,
+                torch_dtype=torch.float16,
+                device_map="auto",
+            )
+
         sequences = self.pipeline(
             LLAMA2_TEMPLATE.format(system_prompt=system, user_msg_1=user),
             do_sample=True,
