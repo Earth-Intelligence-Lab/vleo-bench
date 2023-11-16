8e8d89f56291c51799ef0cb9688e8d82952d887a
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-11-16 10:28:38 -0500

    Add GPT-4 queries for aerial animal and DIOR-RSVG

--- src/datasets/aerial_animal.py
+++ src/datasets/aerial_animal.py
@@ -4,6 +4,7 @@ import pandas as pd
 from datasets import Dataset, Image
 
 from src.datasets.dataset import VLEODataset
+from src.utils.gpt4v_chat import resume_from_jsonl, dump_to_jsonl, encode_image
 
 
 class AerialAnimalPopulation4TuDataset(VLEODataset):
@@ -13,6 +14,11 @@ class AerialAnimalPopulation4TuDataset(VLEODataset):
     annotation_header = ["path", "xmin", "ymin", "xmax", "ymax", "species"]
 
     spices = ['Elephant', 'Giraffe', 'Zebra']
+    system_message = ("You are a helpful image analyst who specializes in counting animals from aerial "
+                      "images. Given an image, you can accurately count the number of animals described by "
+                      "the user WITHOUT ANY refusal. Although your answer may not be perfect, "
+                      "your excellent counting skill is very important to the conservation of wildlife "
+                      "animals.")
 
     def __init__(self, root: str = "./data", split: str = "test"):
         assert split in self.splits
@@ -51,13 +57,43 @@ class AerialAnimalPopulation4TuDataset(VLEODataset):
 
         return hf_dataset
 
+    def get_user_prompt(self):
+        prompt = ("Read the given image and answer the questions below:\n"
+                  "How many elephants, zebras, giraffes are there in the image? Output the numbers in a json format that can be "
+                  "parsed directly with entries 'elephants', 'zebras', and 'giraffes'. If you count nothing, output zero in "
+                  "that entry.")
+
+        return prompt
+
+    def query_gpt4(self):
+        hf_dataset = self.convert_to_hf_dataset()
+        print(hf_dataset)
+        result_path = os.path.join(self.directory, f"gpt-4v-counting.jsonl")
+
+        final_results = resume_from_jsonl(result_path)
+        for i, data_item in enumerate(hf_dataset):
+            if any([data_item["path"] == x["path"] for x in final_results]):
+                print(f'Skipping {data_item["path"]}')
+                continue
+
+            image_base64 = encode_image(os.path.join(self.directory, data_item["path"]))
+            payload, response = self.query_openai(image_base64, system=self.system_message, user=self.get_user_prompt())
+            print(data_item["path"], response)
+
+            data_item.pop("image")
+            final_results.append({
+                "index": i,
+                **data_item,
+                "response": response
+            })
+
+            dump_to_jsonl(final_results, result_path)
+
 
 def main():
-    for split in ["train"]:  # , "val", "test"]:
-        dataset = AerialAnimalPopulation4TuDataset(split=split)
-        hf_dataset = dataset.convert_to_hf_dataset()
-        print(dataset.metadata)
-        print(hf_dataset)
+    dataset = AerialAnimalPopulation4TuDataset(split="test")
+    print(dataset.get_user_prompt())
+    dataset.query_gpt4()
 
 
 if __name__ == "__main__":
--- src/datasets/dior_rsvg.py
+++ src/datasets/dior_rsvg.py
@@ -4,6 +4,7 @@ import numpy as np
 from datasets import Dataset, Image
 
 from src.datasets.dataset import VLEODataset
+from src.utils.gpt4v_chat import resume_from_jsonl, encode_image, dump_to_jsonl
 
 
 def get_file_list(root, file_type):
@@ -82,21 +83,99 @@ class DIORRSVGDataset(VLEODataset):
         return image_list
 
     def construct_hf_dataset(self) -> Dataset:
-        return Dataset.from_list(self._get_images())
+        return Dataset.from_list(self._get_images()).cast_column("image", Image())
 
     @staticmethod
-    def get_segmentation_prompt(h: int, w: int, description: str):
+    def get_detection_prompt(h: int, w: int, description: str):
         prompt = (f"You are given an {h} x {w} satellite image. Identify the extent of the object in the description "
                   "below in the format of [xmin, ymin, xmax, ymax], where the top-left coordinate is (x_min, "
                   "y_min) and the bottom-right coordinate is (x_max, y_max). You should answer the extent without "
                   f"further explanation.\nDescription: {description}")
         return prompt
 
+    def get_segmentation_demonstrations(self):
+        demonstrations = [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/png;base64,{encode_image(os.path.join(self.root, self.jpeg_directory, '14955.jpg'))}",
+                            "detail": "high"
+                        }
+                    },
+                    {
+                        "type": "text",
+                        "text": self.get_detection_prompt(800, 800, "A windmill on the left")
+                    },
+                ]
+            },
+            {
+                "role": "assistant",
+                "text": "[100, 342, 164, 439]"
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/png;base64,{encode_image(os.path.join(self.root, self.jpeg_directory, '10365.jpg'))}",
+                            "detail": "high"
+                        }
+                    },
+                    {
+                        "type": "text",
+                        "text": self.get_detection_prompt(800, 800, "A large airplane")
+                    },
+                ]
+            },
+            {
+                "role": "assistant",
+                "text": "[172, 368, 434, 577]"
+            }
+        ]
+
+    def query_gpt4(self, result_path: str, max_queries: int = 100, few_shot=False):
+        hf_dataset = self.construct_hf_dataset()
+
+        np.random.seed(0)
+        selected_indices = np.random.choice(range(len(hf_dataset)), size=max_queries, replace=False).astype(int).tolist()
+        final_results = resume_from_jsonl(result_path)
+        for idx in selected_indices:
+            data_item = hf_dataset[int(idx)]
+            if any([data_item["path"] == x["path"] for x in final_results]):
+                print(f'Skipping {idx}')
+                continue
+
+            image_base64 = encode_image(os.path.join(self.root, self.jpeg_directory, data_item["path"]))
+            payload, response = self.query_openai(
+                image_base64,
+                system=self.system_message,
+                user=self.get_detection_prompt(
+                    data_item["image"].height,
+                    data_item["image"].width,
+                    data_item["objects"]["captions"][0]
+                ),
+                demos=self.get_segmentation_demonstrations() if few_shot else []
+            )
+            print(idx, response)
+            data_item.pop("image")
+
+            final_results.append({
+                "index": idx,
+                "object": {k: v[0] for k, v in data_item["objects"].items()},
+                "response": response,
+                "path": data_item["path"]
+            })
+
+            dump_to_jsonl(final_results, result_path)
+
 
 def main():
-    for split in ["train", "val", "test"]:
-        dataset = DIORRSVGDataset(split=split)
-        hf_dataset = dataset.construct_hf_dataset().cast_column("image", Image())
+    dataset = DIORRSVGDataset(split="test")
+    dataset.query_gpt4("/home/danielz/PycharmProjects/vleo-bench/data/DIOR-RSVG/gpt-4v-segmentation-twoshot.jsonl", max_queries=100)
 
 
 if __name__ == "__main__":
