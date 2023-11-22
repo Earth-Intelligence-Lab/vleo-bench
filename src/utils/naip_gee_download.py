4383b48db7143b5984ea2eba7dcc5939851345e8
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-11-22 16:45:41 -0500

    Refactor GEE downloads into functions

--- src/utils/naip_gee_download.py
+++ src/utils/naip_gee_download.py
@@ -4,17 +4,17 @@ import geopandas as gpd
 
 ee.Initialize(project="ee-chenhui5")
 
-shapefile_path = '/home/danielz/PycharmProjects/vleo-bench/data/Landmarks/all_unions_polygons_convexhulls_metadata_us.gpkg'
+shapefile_path = './data/Landmarks/Landmarks_OSM_all_unions_polygons_convexhulls_metadata_us.zip'
 shp = gpd.read_file(shapefile_path).to_crs(4326).iloc[[282, 283, 284]]
 shp_bounds = shp.to_crs(3857).bounds.to_numpy()
 
 
-def export_image(geom, idx, bounds, target_width=1920, target_height=1080):
-    roi = ee.Geometry.Polygon(geom)
+def export_image(polygon, index, bounds, target_width=1920, target_height=1080):
+    roi = ee.Geometry.Polygon(polygon)
     xmin, ymin, xmax, ymax = bounds
     bbox_width = (xmax - xmin) / 0.6
     bbox_height = (ymax - ymin) / 0.6
-    
+
     aspect_ratio = bbox_width / bbox_height
     if aspect_ratio > (target_width / target_height):
         # Bounding box is wider than the target aspect ratio
@@ -22,13 +22,13 @@ def export_image(geom, idx, bounds, target_width=1920, target_height=1080):
     else:
         # Bounding box is taller than the target aspect ratio
         scale = bbox_height / target_height
-        
-    scale = max(scale * 0.6, 0.6)    
-    
+
+    scale = max(scale * 0.6, 0.6)
+
     collection = (ee.ImageCollection('USDA/NAIP/DOQQ')
-                   .filterBounds(roi)
-                   # .filterDate('2019-01-01T00:00:00', '2023-12-31T23:59:59')
-                   .sort('system:time_start', False))
+                  .filterBounds(roi)
+                  .filterDate('2019-01-01T00:00:00', '2023-12-31T23:59:59')
+                  .sort('system:time_start', False))
 
     collection_info = collection.getInfo()
     if not collection_info["bands"]:
@@ -37,26 +37,31 @@ def export_image(geom, idx, bounds, target_width=1920, target_height=1080):
         return
 
     image = collection.select(["R", "G", "B"]).mosaic()
-    
+
     task = ee.batch.Export.image.toCloudStorage(
-        fileNamePrefix=f"Landmarks/NAIP_Scaled/NAIP_{idx}",
-        image=image.clip(roi.bounds()),
-        description=f"NAIP_{idx}",
+        fileNamePrefix=f"Landmarks/NAIP_Scaled/NAIP_{index}",
+        image=image.clip(roi),
+        description=f"NAIP_{index}",
         bucket="vleo_benchmark",
         maxPixels=1e13,
-        region=roi, # .getInfo()
+        region=roi,
         scale=scale,
     )
     task.start()
 
-    print(f"Started download task for polygon {idx}.")
-    
+    print(f"Started download task for polygon {index}.")
+
     return task
 
 
-tasks = []
-for idx, geometry, bbox in tqdm(zip(shp["wikidata_entity_id"], shp["geometry"], shp_bounds), total=len(shp)):
-    geom = geometry.__geo_interface__['coordinates']
-    tasks.append(export_image(geom, idx, bbox))
+def main():
+    tasks = []
+    for idx, geometry, bbox in tqdm(zip(shp["entity_id"], shp["geometry"], shp_bounds), total=len(shp)):
+        geom = geometry.__geo_interface__['coordinates']
+        tasks.append(export_image(geom, idx, bbox))
 
-print("All tasks initiated.")
+    print("All tasks initiated.")
+
+
+if __name__ == "__main__":
+    main()
