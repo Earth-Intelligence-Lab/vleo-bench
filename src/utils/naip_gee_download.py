331858a53f3a3ee6f95b6788c06c0c9770f2401f
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-11-23 01:04:33 -0500

    Finalize GEE image export scaling

--- src/utils/naip_gee_download.py
+++ src/utils/naip_gee_download.py
@@ -1,30 +1,18 @@
+import os
 import ee
 from tqdm import tqdm
 import geopandas as gpd
 
-ee.Initialize(project="ee-chenhui5")
 
-shapefile_path = './data/Landmarks/Landmarks_OSM_all_unions_polygons_convexhulls_metadata_us.zip'
-shp = gpd.read_file(shapefile_path).to_crs(4326).iloc[[282, 283, 284]]
-shp_bounds = shp.to_crs(3857).bounds.to_numpy()
-
-
-def export_image(polygon, index, max_dimension=1024):
-    roi = ee.Geometry.Polygon(polygon)
+def export_image(polygon, index, max_dimension=2048):
+    roi = ee.Geometry.Polygon(polygon, proj="EPSG:4326")
 
     collection = (ee.ImageCollection('USDA/NAIP/DOQQ')
-                  .filterBounds(roi)
-                  .filterDate('2019-01-01T00:00:00', '2023-12-31T23:59:59')
+                  .filterBounds(roi.bounds())
+                  .filterDate('2018-01-01T00:00:00', '2023-12-31T23:59:59')
                   .sort('system:time_start', False))
 
-    collection_info = collection.getInfo()
-    if not collection_info["bands"]:
-        print("Empty image!")
-        print(roi.getInfo())
-        return
-
-    image = collection.select(["R", "G", "B"]).mosaic().clipToBoundsAndScale(roi, maxDimension=max_dimension)
-    image_scale = image.select('R').projection().nominalScale()
+    image = collection.select(["R", "G", "B"]).mosaic()
 
     task = ee.batch.Export.image.toCloudStorage(
         fileNamePrefix=f"Landmarks/NAIP_Scaled/NAIP_{index}",
@@ -32,8 +20,9 @@ def export_image(polygon, index, max_dimension=1024):
         description=f"NAIP_{index}",
         bucket="vleo_benchmark",
         maxPixels=1e13,
-        region=roi,
-        scale=image_scale,
+        region=roi.bounds(),
+        crs="EPSG:3857",
+        dimensions=max_dimension,
     )
     task.start()
 
@@ -43,10 +32,16 @@ def export_image(polygon, index, max_dimension=1024):
 
 
 def main():
+    ee.Initialize(project=os.environ["EE_PROJECT"])
+
+    shapefile_path = './data/Landmarks/Landmarks_OSM_all_unions_polygons_convexhulls_metadata_us.zip'
+    shp = gpd.read_file(shapefile_path).to_crs(4326)
+    shp_bounds = shp.to_crs(3857).bounds.to_numpy()
+
     tasks = []
     for idx, geometry, bbox in tqdm(zip(shp["entity_id"], shp["geometry"], shp_bounds), total=len(shp)):
-        geom = geometry.__geo_interface__['coordinates']
-        tasks.append(export_image(geom, idx, bbox))
+        geom = geometry.__geo_interface__['coordinates'][0]
+        tasks.append(export_image(geom, idx))
 
     print("All tasks initiated.")
 
