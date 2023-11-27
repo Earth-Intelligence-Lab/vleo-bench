b98bc59be683a2fc472d86a0347d50b8983eed6f
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-11-27 12:42:57 -0500

    Add PatternNet prompt and queries

--- src/datasets/pattern_net.py
+++ src/datasets/pattern_net.py
@@ -1,9 +1,11 @@
 import os
 from glob import glob
 
+import numpy as np
 from datasets import Dataset, Image
 
 from src.datasets.dataset import VLEODataset
+from src.utils.gpt4v_chat import resume_from_jsonl, dump_to_jsonl, encode_pil_image, encode_image
 
 
 class PatternNetDataset(VLEODataset):
@@ -73,7 +75,49 @@ class PatternNetDataset(VLEODataset):
     filename = "PatternNet.zip"
     directory = os.path.join("PatternNet", "images")
 
-    def __init__(self, credential_path: str, root: str = "data", download: bool = False, checksum: bool = False) -> None:
+    categories = [
+        'airplane',
+        'baseball_field',
+        'basketball_court',
+        'beach',
+        'bridge',
+        'cemetery',
+        'chaparral',
+        'christmas_tree_farm',
+        'closed_road',
+        'coastal_mansion',
+        'crosswalk',
+        'dense_residential',
+        'ferry_terminal',
+        'football_field',
+        'forest',
+        'freeway',
+        'golf_course',
+        'harbor',
+        'intersection',
+        'mobile_home_park',
+        'nursing_home',
+        'oil_gas_field',
+        'oil_well',
+        'overpass',
+        'parking_lot',
+        'parking_space',
+        'railway',
+        'river',
+        'runway',
+        'runway_marking',
+        'shipping_yard',
+        'solar_panel',
+        'sparse_residential',
+        'storage_tank',
+        'swimming_pool',
+        'tennis_court',
+        'transformer_station',
+        'wastewater_treatment_plant'
+    ]
+
+    def __init__(self, credential_path: str, root: str = "data", download: bool = False,
+                 checksum: bool = False) -> None:
         """Initialize a new PatternNet dataset instance.
 
         Args:
@@ -91,6 +135,8 @@ class PatternNetDataset(VLEODataset):
             folder: " ".join([os.path.basename(x).capitalize() for x in folder.split("_")]) for folder in self.folders
         }
 
+        self.categories_normalized = [" ".join([y.capitalize() for y in x.split("_")]) for x in self.categories]
+
     def construct_hf_dataset(self):
         hf_dict = {"image": [], "label": [], "path": []}
         for folder in self.folders:
@@ -105,18 +151,54 @@ class PatternNetDataset(VLEODataset):
         return Dataset.from_dict(hf_dict)
 
     def get_user_prompt(self):
-        prompt = ("You are given a satellite image and a list of land usage types or object names. Choose one option "
-                  "that best describes the image. A list of possible land use types:\n")
+        prompt = ("You are given a satellite image and a list of land usage types or object names. Classify the image "
+                  "into one of the following options. Choose the best option that describes the given image. A list "
+                  "of possible options:\n")
         prompt += ";\n".join([f"{i + 1}. {option}" for i, option in enumerate(self.categories_normalized)])
-        prompt += "\nYour choice of one land use type that best describes the image:"
+        prompt += "\nYour choice of one option that best describes the image:"
 
         return prompt
+
+    def query_gpt4(self, result_path: str, max_queries: int = 1000):
+        hf_dataset = self.construct_hf_dataset()
+        np.random.seed(0)
+        selected_indices = set(np.random.choice(range(len(hf_dataset)), size=max_queries, replace=False).tolist())
+        type_count = {x: 0 for x in self.categories_normalized}
+        print(type_count)
+        type_threshold = 26  # max_queries // len(self.categories)
+
+        final_results = resume_from_jsonl(result_path)
+        for result in final_results:
+            type_count[result["label"]] += 1
+        for idx in selected_indices:
+            data_item = hf_dataset[int(idx)]
+            image_category = data_item["label"]
+
+            if any([idx == x["index"] for x in final_results]):
+                print(f'Skipping {data_item} since it is finished')
+                continue
+            if type_count[image_category] >= type_threshold:
+                print(f'Skipping {data_item} due to class balancing')
+                continue
+
+            image_base64 = encode_image(data_item["image"])
+            payload, response = self.query_openai(image_base64, system=self.system_message, user=self.get_user_prompt())
+            print(idx, response)
+            data_item.pop("image")
+            final_results.append({
+                "index": idx,
+                **data_item,
+                "response": response
+            })
+
+            type_count[image_category] += 1
+            dump_to_jsonl(final_results, result_path)
 
 
 def main():
     dataset = PatternNetDataset(credential_path=".secrets/openai.jsonl")
     print(dataset.folders, dataset.folder2class)
-    hf_dataset = dataset.construct_hf_dataset().cast_column("image", Image())
+    dataset.query_gpt4("/home/danielz/PycharmProjects/vleo-bench/data/PatternNet/gpt-4v.jsonl", max_queries=2000)
 
 
 if __name__ == "__main__":
