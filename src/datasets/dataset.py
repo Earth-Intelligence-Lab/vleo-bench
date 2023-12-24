de28aad5b2ca5def329dc2f1375783a53b3e2461
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-12-24 14:38:21 -0500

    Change daily query limit

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -3,7 +3,7 @@ import os
 from abc import ABC
 from typing import List, Union
 import datetime
-
+import asyncio
 import openai
 import pandas as pd
 import requests
@@ -12,6 +12,8 @@ from tenacity import (
     stop_after_attempt,
     wait_random_exponential,
 )
+
+from src.utils.gpt4v_chat import base64_image_to_bytes
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -46,7 +48,7 @@ class VLEODataset(ABC):
                 self.credentials["usage"] = 0
 
         self.credential_path = credential_path
-        self.get_openai_credentials()
+        # self.get_openai_credentials()
 
     def _update_usage(self):
         self.credentials.reset_index().to_json(
@@ -61,14 +63,14 @@ class VLEODataset(ABC):
     def get_openai_credentials(self):
         if (self.openai_api_org and
                 self.credentials.groupby(["OpenAI-Organization"]).first().loc[
-                    self.openai_api_org, "usage"] < 100):
+                    self.openai_api_org, "usage"] < 500):
             return self.openai_api_key, self.openai_api_org
 
         if os.environ.get("OPENAI_API_ORG"):
             openai_api_org = os.environ["OPENAI_API_ORG"]
             openai_api_key = os.environ["OPENAI_API_KEY"]
         else:
-            available_orgs = self.credentials[self.credentials["usage"] < 100].sort_values(by="usage", ascending=True)
+            available_orgs = self.credentials[self.credentials["usage"] < 500].sort_values(by="usage", ascending=True)
             openai_api_org = available_orgs.index.tolist()[0]
             openai_api_key = available_orgs.iloc[0]["key"]
 
@@ -80,7 +82,8 @@ class VLEODataset(ABC):
         return openai_api_key, openai_api_org
 
     @retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
-    def query_openai(self, base64_image: Union[List[str], str], system: str, user: str, t: float = 0.2, max_tokens: int = 300,
+    def query_openai(self, base64_image: Union[List[str], str], system: str, user: str, t: float = 0.2,
+                     max_tokens: int = 300,
                      demos=None):
         if demos is None:
             demos = []
@@ -100,31 +103,31 @@ class VLEODataset(ABC):
 
         if len(base64_image) == 1:
             user_message = [
-                        {
-                            "type": "text",
-                            "text": user
-                        },
-                        {
-                            "type": "image_url",
-                            "image_url": {
-                                "url": f"data:image/png;base64,{base64_image}",
-                                "detail": "high"
-                            }
-                        }
-                    ]
+                {
+                    "type": "text",
+                    "text": user
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/png;base64,{base64_image}",
+                        "detail": "high"
+                    }
+                }
+            ]
         else:
             user_message = [{
-                            "type": "image_url",
-                            "image_url": {
-                                "url": f"data:image/png;base64,{x}",
-                                "detail": "high"
-                            }
-                        } for x in base64_image] + [
-                        {
-                            "type": "text",
-                            "text": user
-                        },
-                    ]
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/png;base64,{x}",
+                    "detail": "high"
+                }
+            } for x in base64_image] + [
+                               {
+                                   "type": "text",
+                                   "text": user
+                               },
+                           ]
 
         payload = {
             "model": "gpt-4-vision-preview",
@@ -154,7 +157,7 @@ class VLEODataset(ABC):
         if not response.ok:
             if "RPD" in str(response.json()):
                 print(f"Rate limit exceeded. Reset in {reset_time}\n" + str(response.json()))
-                self.credentials.loc[openai_api_org, "usage"] = 100
+                self.credentials.loc[openai_api_org, "usage"] = 500
                 retry_response = self.query_openai(base64_image, system, user, t, max_tokens, demos)
                 return retry_response
             else:
@@ -162,7 +165,7 @@ class VLEODataset(ABC):
         else:
             reset_time = response.headers["x-ratelimit-reset-requests"]
             print(f"{remaining_requests} queries left for {openai_api_org}. Reset in {reset_time}")
-            self.credentials.loc[openai_api_org, "usage"] = 100 - remaining_requests
+            self.credentials.loc[openai_api_org, "usage"] = 500 - remaining_requests
             self._update_usage()
 
         return payload, response.json()
