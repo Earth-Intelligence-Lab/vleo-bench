24d38341d0ea89b77c0c9d0a2dcc4e65c4f67db9
danielz02 <31590926+danielz02@users.noreply.github.com>
2023-12-24 14:48:26 -0500

    Add counting plots

---
+++ src/utils/plot_counting.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+from src.datasets.neon_trees import evaluation as neon_eval
+from src.datasets.cowc import evaluation as cowc_eval
+from src.datasets.aerial_animal import evaluation as animal_eval
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(font_scale=2)
+
+fig, axes = plt.subplots(ncols=3, nrows=1, figsize=(18, 6))
+neon_eval("./data/NeonTreeEvaluation/gpt-4v-counting.jsonl", axes[0])
+cowc_eval("./data/cowc-m/gpt-4v-combined.jsonl", axes[1])
+animal_eval("./data/aerial-animal-population-4tu/gpt-4v-counting.jsonl", axes[2])
+axes[0].set_title("Neon Tree")
+axes[1].set_title("COWC Vehicle")
+axes[2].set_title("Aerial Animal")
+
+plt.tight_layout()
+plt.savefig("./data/NeonTreeEvaluation/counting-scatter-comparison.pdf")
